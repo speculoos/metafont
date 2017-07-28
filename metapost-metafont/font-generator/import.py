f082e149c890d5e63d4664e88afa497b4a0c19b6
Antoine-Gelgon <antoine.gelgon@gmail.com>
2017-07-28 13:09:01 +0200

    generate font

--- metapost-metafont/font-generator/import.py
+++ metapost-metafont/font-generator/import.py
@@ -6,12 +6,23 @@ import sys
 
 SVG_DIR = glob.glob('svg/clean/*.svg')
 font = fontforge.open('tmp/empty.sfd')
+line_chasse = ' width="'
 
 for glyph in SVG_DIR:
     letter = glyph.split("/")[-1].replace(".svg", "")
     print(letter)
-    letter_char= font.createChar(int(letter))
-    letter_char.width = 611
+
+    svg_file = open(glyph, "r")
+    for ligne in svg_file:
+        if line_chasse in ligne:
+            val_chasse = ligne.split('width="')[-1].split('"')[-2]
+            # val_chasse = (float(val_chasse) / 2) / 1.075
+            val_chasse = float(val_chasse ) * 1.736;
+            print('la valeooour des de', val_chasse)
+    svg_file.close()
+
+    letter_char = font.createChar(int(letter))
+    letter_char.width = float(val_chasse)
     letter_char.importOutlines(glyph)
 
 font.generate('FINAL/metafranquin.sfd')
